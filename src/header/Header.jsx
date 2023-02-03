3758642a620a1573c9c6fa846d92c82dcd2b7ddd
Olena Ryabenko <olenaryabenko@gmail.com>
2023-02-03 13:18:18 +0200

    new imgs

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,20 +3,21 @@ import MenuItem from './MenuItem';
 import './header.scss';
 
 const headerItemsList = [
-	{ title: 'For passangers', number: 1 },
-	{ title: 'IEV Services', number: 2 },
-	{ title: 'VIP', number: 3 },
-	{ title: 'Corporate', number: 4 },
-	{ title: 'Press Room', number: 5 },
+	{ title: 'For passangers', id: 1 },
+	{ title: 'IEV Services', id: 2 },
+	{ title: 'VIP', id: 3 },
+	{ title: 'Corporate', id: 4 },
+	{ title: 'Press Room', id: 5 },
+	{ title: 'EN', id: 6 },
 ];
 
 const Header = () => {
 	return (
 		<section className='header'>
-			<img className='header__logo' src='./img/logo.png' alt='logo'></img>
+			<img className='header__logo' src='.\img\logo_logo.png' alt='logo'></img>
 			<ul className='header_content-list'>
 				{headerItemsList.map((el) => {
-					return <MenuItem key={el.number} title={el.title} />;
+					return <MenuItem key={el.id} title={el.title} />;
 				})}
 			</ul>
 		</section>
